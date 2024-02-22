0403ed8d66f0921d2d328ff3f0122965726ed2fb
De Roover Justin <justinderoover@gmail.com>
2024-02-22 17:30:24 +0100

    setup gsap

--- src/components/SectionFirst/SectionFirst.jsx
+++ src/components/SectionFirst/SectionFirst.jsx
@@ -1,58 +1,87 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 function SectionFirst() {
-  useGSAP((context) => {
-    gsap.from(
-      ".container-first h1 span",
-      1,
-      {
-        top: 50,
+  useGSAP(() => {
+    const tl = gsap.timeline({ paused: true });
+
+    tl.add(
+      gsap.from(".container-first h1 span", {
+        duration: 1,
+        y: 50,
         opacity: 0,
         ease: "power2.out",
         stagger: 0.5,
-      },
-      0.3
-    );
-    gsap.from(
-      ".btn-first",
-      1,
-      { top: -50, opacity: 0, ease: "power2.out", stagger: 0.5 },
-      "-=1"
-    );
-    gsap.from(
-      ".canette",
-      1,
-      { right: -50, opacity: 0, ease: "power2.out", stagger: 0.5 },
-      0.3
-    ),
-    gsap.from(
-        ".lignes .l1",
-        1,
-        { width: 0, ease: "power2.out", stagger: 0.5 },
+      })
+    )
+      .add(
+        gsap.from(".btn-first", {
+          duration: 1,
+          y: -50,
+          opacity: 0,
+          ease: "power2.out",
+          stagger: 0.5,
+        }),
         "-=1"
-    ),
-    gsap.from(
-        ".lignes .l2",
-        1,
-        { width: 0, ease: "power2.out", stagger: 0.5 },
+      )
+      .add(
+        gsap.fromTo(
+          ".canette",
+          { y: 0 },
+          { y: 15, duration: 2, yoyo: true, repeat: -1, ease: "power2.inOut" }
+        ),
+        0
+      )
+      .add(
+        gsap.from(".canette", {
+          duration: 1,
+          right: -50,
+          opacity: 0,
+          ease: "power2.out",
+          stagger: 0.5,
+        }),
+        0.3
+      )
+      .add(
+        gsap.from(".lignes .l1", {
+          duration: 1,
+          width: 0,
+          ease: "power2.out",
+          stagger: 0.5,
+        }),
         "-=1"
-    ),
-    gsap.to(".canette", {
-        rotate: 15,
-        duration: 2.5,
-        ease: ".power2.out",
-    }),
-    gsap.fromTo(
-        ".canette",
-        { y: 0 },
-        { y: 15, duration: 2, yoyo: true, repeat: -1, ease: "power2.inOut" },
-        "0"
-    );
+      )
+      .add(
+        gsap.from(".lignes .l2", {
+          duration: 1,
+          width: 0,
+          ease: "power2.out",
+          stagger: 0.5,
+        }),
+        "-=1"
+      )
+      .add(
+        gsap.fromTo(
+          ".canette",
+          {
+            rotate: 0,
+          },
+          { rotate: 15, duration: 3, ease: "power2.out" },
+          "0"
+        )
+      );
 
-    console.log(context);
+    ScrollTrigger.create({
+      trigger: ".section-first",
+      start: "top center",
+      end: "bottom center",
+      scrub: true,
+      onEnter: () => tl.restart(),
+      onEnterBack: () => tl.restart(),
+    });
   }, []);
 
   return (
