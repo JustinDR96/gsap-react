9deccd278b4a80b329f836ea14701a6a45bd7482
De Roover Justin <justinderoover@gmail.com>
2024-02-24 17:16:22 +0100

    fic

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,30 +1,15 @@
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
-gsap.registerPlugin(useGSAP);
-
 function SectionTitle() {
-  useGSAP(() => {
-    gsap.to(".text ul", {
-      x: "100%", // se déplace vers la droite de l'écran
-      duration: 15, // ajustez la durée selon vos besoins
-      ease: "none", // animation linéaire
-      repeat: -1, // répétez l'animation
-      delay: 0,
-    });
-  });
-
   return (
     <div className="section-title">
       <div className="text">
-        <ul>
-          <li>title 1</li>
-          <li>title 2</li>
-          <li>title 3</li>
-          <li>title 4</li>
-          <li>title 5</li>
-          <li>title 6</li>
-        </ul>
+        <div className="marquee-content">
+          <span>title 1</span>
+          <span>title 2</span>
+          <span>title 3</span>
+          <span>title 4</span>
+          <span>title 5</span>
+          <span>title 6</span>
+        </div>
       </div>
     </div>
   );
