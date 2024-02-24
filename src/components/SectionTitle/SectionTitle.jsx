d677f5b282bf464275dcc37d76cf207d142bd26f
De Roover Justin <justinderoover@gmail.com>
2024-02-24 18:12:53 +0100

    slider infinite

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,15 +1,45 @@
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
 function SectionTitle() {
+  const marqueeRef = useRef(null);
+
+  useGSAP(() => {
+    const marquee = marqueeRef.current;
+    const clones = [...marquee.children, ...marquee.children];
+
+    gsap.set(marquee, {
+      css: {
+        width: `${clones.length * 30}%`,
+      },
+    });
+
+    gsap.set(clones, {
+      css: {
+        width: `${100 / clones.length}%`,
+      },
+    });
+
+    gsap.to(clones, {
+      x: "-200%",
+      ease: "none",
+      repeat: -1,
+      duration: 10,
+    });
+  });
+
   return (
     <div className="section-title">
-      <div className="text">
-        <div className="marquee-content">
-          <span>title 1</span>
-          <span>title 2</span>
-          <span>title 3</span>
-          <span>title 4</span>
-          <span>title 5</span>
-          <span>title 6</span>
-        </div>
+      <div className="text" ref={marqueeRef}>
+        <span>title 1</span>
+        <span>title 2</span>
+        <span>title 3</span>
+        <span>title 4</span>
+        <span>title 5</span>
+        <span>title 6</span>
       </div>
     </div>
   );
